2b97569b05befa708edb1f03c2d4ff3126b8ce93
Ihor Khrystiuk <inanutshell86@gmail.com>
2016-05-24 20:25:05 +0300

    hello

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/hello.py hello:app
+sudo ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
 cd /home/box/web/
